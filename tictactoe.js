e0551251b75ee881abd08e5a86d121aafd51254f
Godfrey <godfreyjr@gmail.com>
2018-01-10 01:20:15 -0600

    Update to show player move and attempt to show cpu move

--- tictactoe.js
+++ tictactoe.js
@@ -1,48 +1,60 @@
 
 var playerMarker = '';
 var cpuMarker = '';
+var gameStarted = false;
 var playerTurn = true;
 var boxSelected = '';
 
 
 $("#startBtn").click(function(event) {
-    runSetup();
+    if (!gameStarted){
+        runSetup();
+    }
 });
 
 function runSetup() {
-    $('#setup').text('Would you like to be X or O?');
-    var xButton = $('<button id="xButton"></button>').text('X');
-    var oButton = $('<button id="oButton"></button>').text('O');
+    $('#setup').text('Choose your mark: ');
+    var xButton = $('<button class="marker" id="xButton"></button>').text('X');
+    var oButton = $('<button class="marker" id="oButton"></button>').text('O');
     $('#setup').append(xButton, oButton);
 
-    $("#setup").click(function(event) {
+    $(".marker").click(function(event) {
         if (event.target.id === "xButton") {
             playerMarker = 'X';
             cpuMarker = 'O';
-            $('#setup').text('You have selected '+playerMarker+', lets begin!');
         }
         if (event.target.id === 'oButton') {
             playerMarker = 'O';
             cpuMarker = 'X';
-            $('#setup').text('You have selected '+playerMarker+', lets begin!');
         }
+        $('#setup').text('You have selected '+playerMarker+', lets begin!');
+        gameStarted = true;
     });
 }
 
-$(".wrapper").click(function(event) {
-    var boxSelected = event.target.id;
-})
+$(".box").click(function(event) {
+    if (gameStarted) {
+        if (playerTurn) {
+            boxSelected = event.target.id;
+            $('#'+boxSelected).text(playerMarker);
+            playerTurn = !playerTurn;
+        }
 
-function gamePlay() {
-    if (playerTurn) {
-        $(boxSelected).text(playerMarker);
+        boxSelected = cpuMove();
+        $('#'+boxSelected).text(cpuMarker);
+        playerTurn = !playerTurn;
     }
-    else {
-        cpuMove();
-    }
-
-}
+});
 
 function cpuMove() {
+    var totalBoxes = 9;
+    var availableBoxes = [];
+    for (var i = 0; i < totalBoxes.length; i++) {
+        if ($('#box'+ i).text() === '') {
+            availableBoxes.push('box'+i);
+        }
+    }
 
+    return availableBoxes[0];
 }
+
